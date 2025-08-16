35c3bb67667ceae1826a214dc921bdf20f95b492
Daniel Rudolph <daniel@bsolut.com>
2025-08-16 11:23:11 +0200

    feat(footer): add link to original repo

--- components/Footer.js
+++ components/Footer.js
@@ -3,5 +3,19 @@ import React from "react";
 
 // Footer-Komponente für die Fußzeile der Seite
 export const Footer = () => {
-	return <section className="py-16">Ein Projekt von Lukas Weihrauch</section>;
+	return (
+		<section className="py-16 text-center">
+			<p className="text-gray-600">
+				Ein Projekt von Lukas Weihrauch |{" "}
+				<a
+					href="https://github.com/l2xu/betterbahn"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-blue-600 hover:text-blue-800 underline"
+				>
+					GitHub
+				</a>
+			</p>
+		</section>
+	);
 };
